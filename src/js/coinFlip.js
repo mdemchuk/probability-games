a6da1d5d028bca9d83f4b0f84163f55efe9982f4
mdemchuk <101371223+mdemchuk@users.noreply.github.com>
2022-12-05 20:09:58 -0500

    Add files via upload

--- src/js/coinFlip.js
+++ src/js/coinFlip.js
@@ -1,50 +1,95 @@
+		// determines if coin will be weighted or not
+		var isWeighted = Math.floor(Math.random() * 2) + 1;
+
+		// counters for both sides
+		var headCount = 0;
+		var tailCount = 0;
+
+		// determines amount of weight for the heavier side 
+		// (anywhere from 70-30 to 57-43, with the higher number being the heavier side)
+		var min = 57;
+		var max = 71;
+		var difference = max - min;
+		var weightHeavierSide = Math.floor(Math.random() * difference) + min;
+
+		// determines if tails or heads is the heavier side (1 for heads and 2 for tails)
+		var heavierSide = Math.floor(Math.random() * 2) + 1;
+		
 		// Function to flip coin
 		function flipCoin() {
-			
-			let randomNumber = Math.random();
-			let weighted = Math.floor(Math.random() * 2) + 1;
 
-			// coin is heads
-			if (randomNumber < .5) {
-				document.querySelector("h1").innerHTML = "You got HEADS!";
-				document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
-			} 
-			// coin is tails
+			// coin is weighted
+			if(isWeighted == 1) {
+				// heavier side is heads
+				if(heavierSide == 1) {
+       				let num = Math.floor(Math.random() * 100) + 1;
+					
+					// if weighted coin is heads
+        			if (num <= weightHeavierSide) {
+          				document.querySelector("h1").innerHTML = "You got HEADS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+						headCount += 1;
+        			} 
+					// if weighted coin is tails
+					else {
+						document.querySelector("h1").innerHTML = "You got TAILS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+						tailCount += 1;
+        			}
+      				
+      				document.getElementById("headTotal").innerHTML = headCount;
+					document.getElementById("tailTotal").innerHTML = tailCount;
+				}
+
+				// heavier side is tails
+				else {
+					let num = Math.floor(Math.random() * 100) + 1;
+					// weighted coin is tails
+					if (num <= weightHeavierSide) {
+						document.querySelector("h1").innerHTML = "You got TAILS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+						tailCount += 1;
+					} 
+					// weighted coin is heads
+					else {
+						document.querySelector("h1").innerHTML = "You got HEADS!";
+						document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+						headCount += 1;
+					}
+					document.getElementById("headTotal").innerHTML = headCount;
+					document.getElementById("tailTotal").innerHTML = tailCount;
+				}
+			}
+
+			// coin is not weighted
 			else {
-				document.querySelector("h1").innerHTML = "You got TAILS!";
-				document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+				let num = Math.floor(Math.random() * 100) + 1;
+					
+				// if regular coin is heads
+        		if (num <= 50) {
+					document.querySelector("h1").innerHTML = "You got HEADS!";
+					document.getElementById("coin").setAttribute("src", "../images/coinHead.png");
+          			headCount += 1;
+        		} 
+				// if regular coin is tails
+				else {
+					document.querySelector("h1").innerHTML = "You got TAILS!";
+					document.getElementById("coin").setAttribute("src", "../images/coinTail.png");
+          			tailCount += 1;
+				}
+      			document.getElementById("headTotal").innerHTML = headCount;
+				document.getElementById("tailTotal").innerHTML = tailCount;
 			}
 		}
 
-		var isWeighted = 0;
-
 		// Function to flip coin multiple times
 		function flipCoinMultiple() {
-			// reset answer
-			document.querySelector("h2").innerHTML = "";
-
-			// determines if coin is weighted or not
-			isWeighted = Math.floor(Math.random() * 2) + 1;
 
 			// gets amount of flips from user
 			let qty = document.getElementById("quantity").value;
 
 			// if coin is weighted
-			if (isWeighted == 1) {
-				// counters for both sides
-				let headCount = 0;
-				let tailCount = 0;
-
-				// determines amount of weight for the heavier side 
-				// (anywhere from 70-30 to 57-43, with the higher number being the heavier side)
-				let min = 57;
-				let max = 71;
-				let difference = max - min;
-				let weightHeavierSide = Math.floor(Math.random() * difference) + min;
-
-				// determines if tails or heads is the heavier side (1 for heads and 2 for tails)
-				let heavierSide = Math.floor(Math.random() * 2) + 1;
-								
+			if (isWeighted == 1) {								
       			if(heavierSide == 1) {
 					for (let i = 0; i < qty; i++) {
        					let num = Math.floor(Math.random() * 100) + 1;
@@ -82,9 +127,6 @@
 
 			// if coin is not weighted
 			else {
-				let headCount = 0;
-				let tailCount = 0;
-
       			for (let i = 0; i < qty; i++) {
        				let num = Math.floor(Math.random() * 100) + 1;
 					
@@ -104,14 +146,30 @@
 
 		function ifWeighted() {
 			if(isWeighted == 1)
-				document.querySelector("h2").innerHTML = "You are Correct!";
+				document.querySelector("h1").innerHTML = "You are Correct!";
 			else
-				document.querySelector("h2").innerHTML = "You are Incorrect";
+				document.querySelector("h1").innerHTML = "You are Incorrect";
+
+			document.getElementById("headTotal").innerHTML = 0;
+			document.getElementById("tailTotal").innerHTML = 0;
+			headCount = 0;
+			tailCount = 0;
+			isWeighted = Math.floor(Math.random() * 2) + 1;
+			weightHeavierSide = Math.floor(Math.random() * difference) + min;
+			heavierSide = Math.floor(Math.random() * 2) + 1;
 		}
 
 		function ifNotWeighted() {
 			if(isWeighted == 1)
-				document.querySelector("h2").innerHTML = "You are Incorrect";
+				document.querySelector("h1").innerHTML = "You are Incorrect";
 			else
-				document.querySelector("h2").innerHTML = "You are Correct!";
+				document.querySelector("h1").innerHTML = "You are Correct!";
+
+			document.getElementById("headTotal").innerHTML = 0;
+			document.getElementById("tailTotal").innerHTML = 0;
+			headCount = 0;
+			tailCount = 0;
+			isWeighted = Math.floor(Math.random() * 2) + 1;
+			weightHeavierSide = Math.floor(Math.random() * difference) + min;
+			heavierSide = Math.floor(Math.random() * 2) + 1;
 		}
--- src/js/goatGame.js
+++ src/js/goatGame.js
@@ -9,7 +9,8 @@ Doors[carLocation] = 1;
 var gamesPlayed = 0;
 var gamesWon = 0;
 var gamesLost = 0;
-var keptDoor = 0;
+var keptDoor = false;
+var keptDoorWon = 0;
 var switchDoors = 0;
 var switchDoorGames = 0;
 var keptDoorsGames = 0;
@@ -20,14 +21,11 @@ var choiceMade = 0;
 var globalDoorChose = 0;
 var revealGoat = 0;
 
-var string = "Door Zero: " + Doors[0] + "-- Door One: " + Doors[1] + "-- Door Two: " + Doors[2];
-
 function testModal() {
     $('#infoModal').modal('show');
 }
 
 function playAgain() {
-    //$('#exampleModal').modal('show'); //reveals the modal without button being clicked
     Doors[0] = 0;
     Doors[1] = 0;
     Doors[2] = 0;
@@ -38,13 +36,29 @@ function playAgain() {
     document.getElementById("door0").src = doorImg;
     document.getElementById("door1").src = doorImg;
     document.getElementById("door2").src = doorImg;
-    document.getElementById("buttonDoor0").disabled = false;
-    document.getElementById("buttonDoor1").disabled = false;
-    document.getElementById("buttonDoor2").disabled = false;
 
     document.getElementById("door0").style.boxShadow = "none";
     document.getElementById("door1").style.boxShadow = "none";
     document.getElementById("door2").style.boxShadow = "none";
+
+    document.getElementById("titleSentence").innerHTML = "Welcome to Monty Hall's Problem!"
+    document.getElementById("firstSentenceID").innerHTML = "There are <b>three</b> doors in front of you. There are <b>two</b> goats, and <b>one</b> car";
+    document.getElementById("continueButton").setAttribute("hidden", "hidden");
+    document.getElementById("switchDoorsButton").setAttribute("hidden", "hidden");
+    document.getElementById("keepDoorsButton").setAttribute("hidden", "hidden");
+
+    document.getElementById("amountTimesToSwitch").setAttribute("hidden", "hidden");
+    document.getElementById("continueSimulation").setAttribute("hidden", "hidden");
+    //reveal new input and continue
+    document.getElementById("simulateRunButton").removeAttribute("hidden");
+    document.getElementById("amountTimesToRun").removeAttribute("hidden");
+
+    document.getElementById("door0").setAttribute("onclick", "stepOne(0)");
+    document.getElementById("door1").setAttribute("onclick", "stepOne(1)");
+    document.getElementById("door2").setAttribute("onclick", "stepOne(2)");
+
+
+
 } //end of play again
 
 function stepOne(doorChose) {
@@ -54,19 +68,52 @@ function stepOne(doorChose) {
     while (revealGoat == doorChose || revealGoat == carLocation) {
         revealGoat = Math.floor(Math.random() * 3);
     }
-    var go = "door" + revealGoat;
-    document.getElementById(go).src = goatImg;
+
 
     let remainderDoor = 5;
     remainderDoor = remainderDoor - (doorChose + 1);
     remainderDoor = remainderDoor - (revealGoat + 1);
     doorChoice = remainderDoor;
 
-    //now display modal with custom string
-    document.getElementById("currentDoorChosen").innerHTML = "You choose Door " + (doorChose + 1);
-    document.getElementById("goatLocationDescription").innerHTML = "Goat is located at Door " + (revealGoat + 1);
-    document.getElementById("whatToDoNext").innerHTML = "Would you like to switch to Door " + (doorChoice + 1);
-    $('#goatLocationModal').modal('show');
+    //remove ability to choose doors from imgs
+    var goatDoor = "door" + revealGoat;
+    var switchDoorLoc = "door" + doorChoice;
+    document.getElementById(goatDoor).removeAttribute("onclick");
+    document.getElementById(switchDoorLoc).removeAttribute("onclick");
+
+    document.getElementById("door" + doorChose).style.boxShadow = "0 0 100px greenyellow";
+    setTimeout(afterStepOne, 400, revealGoat);
+}
+
+function afterStepOne(revealGoat) {
+    var go = "door" + revealGoat;
+    document.getElementById(go).src = goatImg;
+    document.getElementById("titleSentence").innerHTML = "Here is a goat!"
+    document.getElementById("firstSentenceID").innerHTML = "This Goat is Located at Door " + (revealGoat + 1);
+    document.getElementById("continueButton").removeAttribute("hidden");
+}
+
+function intermediateStep() {
+    document.getElementById("continueButton").setAttribute("hidden", "hidden");
+    document.getElementById("titleSentence").innerHTML = "What to do next!"
+    document.getElementById("firstSentenceID").innerHTML = "You choose <b>Door " + (globalDoorChose + 1) +
+        "</b><br> There is a Goat located at <b>Door " + (revealGoat + 1) + "</b><br> Would you like to switch to <b>Door " + (doorChoice + 1) + "</b>";
+    document.getElementById("switchDoorsButton").removeAttribute("hidden");
+    document.getElementById("keepDoorsButton").removeAttribute("hidden");
+
+}
+
+function secondIntermediate() {
+    document.getElementById("titleSentence").innerHTML = "Results!"
+    setTimeout(finalFunction, 800);
+}
+
+function midStepTwo(imgType, userDoorChoice) {
+    if (imgType == 1) {
+        document.getElementById(userDoorChoice).src = carImg;
+    } else {
+        document.getElementById(userDoorChoice).src = goatImg;
+    }
 }
 
 function stepTwo(keptDoor) {
@@ -79,9 +126,37 @@ function stepTwo(keptDoor) {
     }
 
     var userDoorChoice = "door" + doorChoice;
+    if (doorChoice == carLocation) {
+        setTimeout(midStepTwo, 400, 1, userDoorChoice);
 
+    } else {
+        setTimeout(midStepTwo, 400, 2, userDoorChoice);
+
+    }
+
+    document.getElementById("door0").style.boxShadow = "none";
+    document.getElementById("door1").style.boxShadow = "none";
+    document.getElementById("door2").style.boxShadow = "none";
     document.getElementById(userDoorChoice).style.boxShadow = "0 0 100px greenyellow";
 
+    updateStats(doorChoice, keptDoor);
+    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoorWon, keptDoorLost);
+
+    if (doorChoice == carLocation) {
+        document.getElementById("firstSentenceID").innerHTML = "You Win!";
+    } else {
+        document.getElementById("firstSentenceID").innerHTML = "You Lose!";
+    }
+
+    //remove IMG clickness
+    document.getElementById("door0").removeAttribute("onclick");
+    document.getElementById("door1").removeAttribute("onclick");
+    document.getElementById("door2").removeAttribute("onclick");
+
+    setTimeout(secondIntermediate, 500);
+}
+
+function finalFunction() {
     if (Doors[0] == 0)
         document.getElementById("door0").src = goatImg;
     else
@@ -96,22 +171,11 @@ function stepTwo(keptDoor) {
         document.getElementById("door2").src = goatImg;
     else
         document.getElementById("door2").src = carImg;
-
-    updateStats(doorChoice, keptDoor);
-    document.getElementById("buttonDoor0").disabled = true;
-    document.getElementById("buttonDoor1").disabled = true;
-    document.getElementById("buttonDoor2").disabled = true;
-    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoor, keptDoorLost);
-
-    if (doorChoice == carLocation) {
-        document.getElementById("results").innerHTML = "You Win!";
-    } else {
-        document.getElementById("results").innerHTML = "You Lose!";
-    }
-    $('#resultsModal').modal('show');
 }
 
 function determineKeep(number) {
+    document.getElementById("switchDoorsButton").setAttribute("hidden", "hidden");
+    document.getElementById("keepDoorsButton").setAttribute("hidden", "hidden");
     if (number == 0)
         isKept = true;
     else
@@ -141,7 +205,7 @@ function updateStats(x, isKept) {
         if (isKept)
             switchDoors++;
         else
-            keptDoor++;
+            keptDoorWon++;
     } else {
 
         if (isKept)
@@ -154,16 +218,65 @@ function updateStats(x, isKept) {
 
 } //end of update stats
 
+function preSimulate() {
+    //hide old input and simulate runs
+    document.getElementById("simulateRunButton").setAttribute("hidden", "hidden");
+    document.getElementById("amountTimesToRun").setAttribute("hidden", "hidden");
+    //reveal new input and continue
+    document.getElementById("amountTimesToSwitch").removeAttribute("hidden");
+    document.getElementById("continueSimulation").removeAttribute("hidden");
+    document.getElementById("simulateInfo").innerHTML = "Enter in the textbox below how many times you would like to switch doors.";
+
+
+}
+
+function changeToHowToPlay(){
+    document.getElementById("mainInfoSection").innerHTML = "Below there are <b>three doors</b>, and there are <b>two goats</b> and"
+    +" <b>one car</b> hidden behind the doors.  <br> You select <b>one door</b> and then <b>a goat</b> is revealed. "+ 
+    " You are then given an option to either <b>keep your current door</b> or <b>switch to the other unopened door</b>. "+
+    " After which the door you choose is revealed. <br> The purpose of this game is to help educate you on probability.";
+
+    document.getElementById("mainTitleSection").innerHTML = "<b>This is how you play our game.</b>";
+    
+
+    document.getElementById("buttonHowToPlay").setAttribute("hidden","hidden");
+
+    document.getElementById("buttonHistory").removeAttribute("hidden");
+}
+
+function changeToHistory() {
+    document.getElementById("mainInfoSection").innerHTML = "Monty Hall was a TV and radio host most famous for hosting the game "+
+    "show Let's Make a Deal which he produced and hosted for many years.The Monty Hall Problem was named after him because of it " +
+    "similarities with Let's Make a Deal.The problem was first posted and solved a letter by Steve Selvin to the American "+
+    "Statistician in 1975. The original problem reading: Suppose you're on a game show, and you're given the choice of three"+
+    " doors: Behind one door is a car; behind the others, goats.You pick a door, say No. 1, and the host, who knows what's behind "+
+    "the doors, opens another door, say No. 3, which has a goat.He then says to you, Do you want to pick door No. 2? Is it to your"+
+    " advantage to switch your choice ?";
+
+    document.getElementById("mainTitleSection").innerHTML = "<b>The History of the Monty Hall Problem</b>";
+
+    document.getElementById("buttonHistory").setAttribute("hidden","hidden");
+
+    document.getElementById("buttonHowToPlay").removeAttribute("hidden");
+}
+
 function simulateGame() {
     var switchDoor;
     var timesPlayed = document.getElementById("amountTimesToRun").value;
 
-    var timesSwitched = document.getElementById("amountToSwitch").value;
-   $('simulationModal').modal('show');
- 
+    var timesSwitched = document.getElementById("amountTimesToSwitch").value;
+
+    document.getElementById("simulateInfo").innerHTML = "Enter how many times would you like to simulate the game in the textbox below.";
+
+    document.getElementById("amountTimesToSwitch").setAttribute("hidden", "hidden");
+    document.getElementById("continueSimulation").setAttribute("hidden", "hidden");
+    //reveal new input and continue
+    document.getElementById("simulateRunButton").removeAttribute("hidden");
+    document.getElementById("amountTimesToRun").removeAttribute("hidden");
+
     switchDoor = isKept;
     for (var i = 0; i < timesPlayed; i++) {
-        playAgain();
+        //playAgain();
         carLocation = Math.floor(Math.random() * 3);
         var userChoice = Math.floor(Math.random() * 3);
         var revealGoat = carLocation;
@@ -186,12 +299,12 @@ function simulateGame() {
         }
 
         if (i == 0) {
-            document.getElementById("buttonDoor0").disabled = false;
-            document.getElementById("buttonDoor1").disabled = false;
-            document.getElementById("buttonDoor2").disabled = false;
             document.getElementById("door0").src = doorImg;
             document.getElementById("door1").src = doorImg;
             document.getElementById("door2").src = doorImg;
+            document.getElementById("door0").removeAttribute("onclick");
+            document.getElementById("door1").removeAttribute("onclick");
+            document.getElementById("door2").removeAttribute("onclick");
         }
 
         if (i == timesPlayed - 1) {
@@ -209,14 +322,10 @@ function simulateGame() {
                 document.getElementById("door2").src = carImg;
             else
                 document.getElementById("door2").src = goatImg;
-
-            document.getElementById("buttonDoor0").disabled = true;
-            document.getElementById("buttonDoor1").disabled = true;
-            document.getElementById("buttonDoor2").disabled = true;
         }
     }//end of for loop
 
-    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoor, keptDoorLost);
+    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoorWon, keptDoorLost);
 }//end of simulate game
 
 
@@ -224,7 +333,7 @@ function resetStats() {
     gamesPlayed = 0;
     gamesWon = 0;
     gamesLost = 0;
-    keptDoor = 0;
+    keptDoorWon = 0;
     switchDoors = 0;
     switchDoorGames = 0;
     keptDoorsGames = 0;
@@ -234,16 +343,13 @@ function resetStats() {
     document.getElementById("door0").src = doorImg;
     document.getElementById("door1").src = doorImg;
     document.getElementById("door2").src = doorImg;
-    document.getElementById("buttonDoor0").disabled = false;
-    document.getElementById("buttonDoor1").disabled = false;
-    document.getElementById("buttonDoor2").disabled = false;
 
     playAgain();
-    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoor, keptDoorLost);
+    printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoorWon, keptDoorLost);
 
 }
 
-function printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoor, keptDoorLost) {
+function printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, switchDoors, switchDoorLost, keptDoorsGames, keptDoorWon, keptDoorLost) {
 
     document.getElementById("gamesPlayed").innerHTML = "Total Games Played: " + gamesPlayed;
     if (gamesWon != 0)
@@ -275,10 +381,10 @@ function printStatistics(gamesPlayed, gamesWon, gamesLost, switchDoorGames, swit
 
 
     document.getElementById("keptDoorsGames").innerHTML = "Total Games where kept door: " + keptDoorsGames;
-    if (keptDoor != 0)
-        document.getElementById("keptDoorsWon").innerHTML = "Kept doors and won: " + keptDoor + " (" + (((1.0 * keptDoor) / keptDoorsGames) * 100).toFixed(2) + "%)";
+    if (keptDoorWon != 0)
+        document.getElementById("keptDoorsWon").innerHTML = "Kept doors and won: " + keptDoorWon + " (" + (((1.0 * keptDoorWon) / keptDoorsGames) * 100).toFixed(2) + "%)";
     else
-        document.getElementById("keptDoorsWon").innerHTML = "Kept doors and won: " + keptDoor + " (0%)";
+        document.getElementById("keptDoorsWon").innerHTML = "Kept doors and won: " + keptDoorWon + " (0%)";
 
     if (keptDoorLost != 0)
         document.getElementById("keptDoorsLose").innerHTML = "Kept doors and lost: " + keptDoorLost + " (" + (((1.0 * keptDoorLost) / keptDoorsGames) * 100).toFixed(2) + "%)";
